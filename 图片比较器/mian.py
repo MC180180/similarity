76a36c810a9990f0e36d9e6af831fe742013fa9d
Love Asuka <154034002+Love-Asuka@users.noreply.github.com>
2025-08-13 19:26:01 +0800

    Delete 图片比较器 directory
    回退

--- 图片比较器/mian.py
+++
@@ -1,8 +0,0 @@
-import app
-from server import app, open_browser
-
-
-    # 启动Flask服务器，并打印标识信息
-print("Starting Flask server on http://127.0.0.1:18200")
-open_browser()
-app.run(debug=False, port=18200)
